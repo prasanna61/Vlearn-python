1db4d9ec95d64ca48f4f25ae232d2319551ad216
LAKSHMI PRASANNA TANGUTURI <88602508+prasanna61@users.noreply.github.com>
2023-04-30 18:30:15 +0530

    Update question2.py

--- question2.py
+++ question2.py
@@ -3,13 +3,14 @@ temp_str=""
 uniq_str=""
 for i in org_str:
     if i not in temp_str:
-        uniq_str+=i
-        asc=ord(i)
-    if i.isupper():
-        b=chr(asc+32)
-    else:
-        b=chr(asc-32)
-    temp_str+=i
+        asc=ord(i) #ascii value
+        if i.isupper(): # if it is uppercase conert into lower case
+            b=chr(asc+32)
+            uniq_str+=b
+        else:
+            uniq_str+=i #lower case add to uniq str
+            b=chr(asc-32)
+    temp_str+=i #add both upper and lower cases to unq str
     temp_str+=b
     
 print(uniq_str)   
